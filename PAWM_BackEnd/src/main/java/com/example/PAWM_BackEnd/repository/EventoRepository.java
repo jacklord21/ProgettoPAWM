fa1a7717edf43d0513f0824608f692673e58dfa5
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-03-11 16:35:58 +0100

    Implementazione metodo per trovare eventi con data successiva a quella corrente

--- PAWM_BackEnd/src/main/java/com/example/PAWM_BackEnd/repository/EventoRepository.java
+++ PAWM_BackEnd/src/main/java/com/example/PAWM_BackEnd/repository/EventoRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.example.PAWM_BackEnd.entita.Evento;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface EventoRepository extends CrudRepository<Evento, Long>
 {
+    List<Evento> findByDataSvolgimentoAfter(LocalDate data);
 }
