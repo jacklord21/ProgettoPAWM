7b1c70ef680d3674240f108b1d9909e15a563d75
Giacomo <giacomo.matteucci@studenti.unicam.it>
2022-03-11 16:37:36 +0100

    Modifica metodo per estarre solamente gli eventi disponibili

--- PAWM_BackEnd/src/main/java/com/example/PAWM_BackEnd/controller/EventoController.java
+++ PAWM_BackEnd/src/main/java/com/example/PAWM_BackEnd/controller/EventoController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 @CrossOrigin()
@@ -19,8 +19,8 @@ public class EventoController
     private EventoRepository er;
 
     @GetMapping("/eventi")
-    public List<Evento> getAll() {
-        return StreamSupport.stream(this.er.findAll().spliterator(), false).collect(Collectors.toList());
+    public List<Evento> getDisponibili() {
+        return new ArrayList<>(this.er.findByDataSvolgimentoAfter(LocalDate.now()));
     }
 
 }
